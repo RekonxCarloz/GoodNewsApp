09e685e2e9dfbc575fcfbaf93da315dcc5edfe1b
Carlos Cobian <rcobianm1600@alumno.ipn.x>
2022-02-03 11:33:55 -0600

    Implementación de Cell para tableview

---
+++ GoodNewsApp/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  GoodNewsApp
+//
+//  Created by Carlos Cobian on 03/02/22.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell{
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+}
